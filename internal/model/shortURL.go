b1a31b10f6be4010daa0ee5c3b004b154b608a04
qzy1233 <3206426114@qq.com>
2025-09-04 16:48:39 +0800

    use uniqueIndex

--- internal/model/shortURL.go
+++ internal/model/shortURL.go
@@ -5,6 +5,6 @@ import "gorm.io/gorm"
 type ShortURL struct {
 	gorm.Model
 	URL         string `gorm:"not null"`
-	ShortCode   string `gorm:"unique"`
+	ShortCode   string `gorm:"uniqueIndex; size:20; not null"`
 	AccessCount int
 }
