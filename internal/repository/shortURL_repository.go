87f801b1d0c52772de0a6050b95261de284f495c
qzy1233 <3206426114@qq.com>
2025-09-04 17:09:43 +0800

    add transaction

--- internal/repository/shortURL_repository.go
+++ internal/repository/shortURL_repository.go
@@ -13,31 +13,35 @@ func NewURLShorteningRepository(db *gorm.DB) *URLShorteningRepository {
 	return &URLShorteningRepository{db: db}
 }
 
-func (ur *URLShorteningRepository) Create(shortURL *model.ShortURL) (*model.ShortURL, error) {
-	err := ur.db.Create(shortURL).Error
-	if err != nil {
-		return nil, err
-	}
-	return shortURL, nil
+func (ur *URLShorteningRepository) WithTx(fn func(repository *URLShorteningRepository) error) error {
+	return ur.db.Transaction(func(tx *gorm.DB) error {
+		return fn(&URLShorteningRepository{db: tx})
+	})
+}
+
+func (ur *URLShorteningRepository) Create(shortURL *model.ShortURL) error {
+	return ur.db.Create(shortURL).Error
 }
 
 func (ur *URLShorteningRepository) GetByShortCode(shortCode string) (*model.ShortURL, error) {
-	shortURL := &model.ShortURL{}
-	err := ur.db.Where("short_code = ?", shortCode).First(shortURL).Error
+	var shortURL model.ShortURL
+	err := ur.db.Where("short_code = ?", shortCode).First(&shortURL).Error
 	if err != nil {
 		return nil, err
 	}
-	return shortURL, nil
+	return &shortURL, nil
 }
 
-func (ur *URLShorteningRepository) Update(shortURL *model.ShortURL) (*model.ShortURL, error) {
-	err := ur.db.Save(shortURL).Error
-	if err != nil {
-		return nil, err
-	}
-	return shortURL, nil
+func (ur *URLShorteningRepository) Update(shortURL *model.ShortURL) error {
+	return ur.db.Save(shortURL).Error
 }
 
-func (ur *URLShorteningRepository) Delete(shortURL *model.ShortURL) error {
-	return ur.db.Delete(shortURL).Error
+func (ur *URLShorteningRepository) DeleteByShortCode(shortCode string) error {
+	tx := ur.db.Where("short_code = ?", shortCode).Delete(&model.ShortURL{})
+	return tx.Error
+}
+
+func (ur *URLShorteningRepository) IncrAccessCount(shortCode string) error {
+	return ur.db.Where("short_code = ?", shortCode).
+		Update("access_count", gorm.Expr("access_count + ?", 1)).Error
 }
